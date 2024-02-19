1d90e6db28abfe17e309b4985507cdf5eb9449e7
Stoker <berdnikov.vasyliy@gmail.com>
2024-02-19 16:37:32 +0800

    version1

--- src/main/java/ru/rusalitc/java/basic/homework13/Client.java
+++ src/main/java/ru/rusalitc/java/basic/homework13/Client.java
@@ -1,2 +1,47 @@
-package ru.rusalitc.java.basic.homework13;public class Client {
+package ru.rusalitc.java.basic.homework13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 8088);
+
+            BufferedReader inMessage = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter outMessage = new PrintWriter(socket.getOutputStream(), true);
+
+            String serverMessage = inMessage.readLine();
+            System.out.println(serverMessage);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите первое число:");
+            String firstValue = scanner.nextLine();
+            float x = Float.parseFloat(firstValue);
+            outMessage.println(x);
+
+            System.out.println("Введите второе число:");
+            String secondValue = scanner.nextLine();
+            float y = Float.parseFloat(secondValue);
+            outMessage.println(y);
+
+            System.out.println("Введите операцию (+, -, *, /):");
+            String mathOp = scanner.nextLine();
+            outMessage.println(mathOp);
+
+            String result = inMessage.readLine();
+            System.out.println(result);
+
+            inMessage.close();
+            outMessage.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/ru/rusalitc/java/basic/homework13/Server.java
+++ src/main/java/ru/rusalitc/java/basic/homework13/Server.java
@@ -1,2 +1,50 @@
-package ru.rusalitc.java.basic.homework13;public class Server {
+package ru.rusalitc.java.basic.homework13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8088);
+
+            Socket socket = serverSocket.accept();
+            System.out.println("Клиент подключен");
+
+            BufferedReader inMessage = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter outMessage = new PrintWriter(socket.getOutputStream(), true);
+            outMessage.println("Вы подключились к серверу к приложению 'Калькулятор', Вам доступны следующие операции: +, -, *, / над двумя числами");
+
+            float x = Float.parseFloat(inMessage.readLine());
+            float y = Float.parseFloat(inMessage.readLine());
+            String mathOp = inMessage.readLine();
+            if (operation(x, y, mathOp).equals("Введена неккоректная операция")) {
+                outMessage.println("Введена неккоректная операция");
+            } else {
+                outMessage.println(x + " " + mathOp + " " + y + " = " + operation(x, y, mathOp));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String operation(float a, float b, String s) {
+        switch (s) {
+            case "+":
+                return Float.toString(a + b);
+            case "-":
+                return Float.toString(a - b);
+            case "*":
+                return Float.toString(a * b);
+            case "/":
+                return Float.toString(a / b);
+            default:
+                return "Введена неккоректная операция";
+        }
+    }
+
 }
